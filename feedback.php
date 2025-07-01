40fb6e21e9dcf6f8eb4c51276774014caaf40da8
Chearow <chearow@outlook.com>
2025-07-01 17:11:56 +0400

    Feedback создание страницы формы

--- feedback.php
+++ feedback.php
@@ -20,7 +20,47 @@ requireLogin();
         <input type="text" class=form-control" id="subject" name="subject" required>
     </div>
 
+    <div class="mb-3">
+        <label class="form-label" for="message">Сообщение</label>
+        <textarea class="form-control" name="message" id="message" rows="4" required></textarea>
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Было ли Вам удобно пользоваться нашим сайтом?</label><br>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type=radio name="radio_option" id="radio1" value="Да" required>
+            <label class="form-check-label" for="radio1">Да</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="radio_option" id="radio2" value="Нет">
+            <label class="form-check-label" for="radio2">Нет</label>
+        </div>
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Какие глобальные моменты, на Ваш взгляд, требуют доработки?</label><br>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" name="checkbox_options[]" id="cb1" value="Визуал">
+            <label class="form-check-label" for="cb1">Визуал</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" name="checkbox_options[]" id="cb2" value="Функционал">
+            <label class="form-check-label" for="cb2">Функционал</label>
+        </div>
+    </div>
+
+    <div class="mb-3">
+        <label for="dropdown_option" class="form-label">Тип обращения</label>
+        <select class="form-select" id="dropdown_option" name="dropdown_option" required>
+            <option value="">Выберите...</option>
+            <option value="Вопрос">Вопрос</option>
+            <option value="Ошибка">Ошибка</option>
+            <option value="Предложение">Предложение</option>
+        </select>
+    </div>
+
     <button type="submit" name="send" class="btn btn-primary">Отправить</button>
+    <button type="reset" class="btn btn-secondary">Сбросить</button>
 </form>
 </body>
 </html>
